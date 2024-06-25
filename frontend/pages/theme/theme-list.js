ba3f4675b98adf625d34c81edf1c83ed3fafd2ab
Ireneowo <renpinning@gmail.com>
2024-06-25 14:07:47 +0800

    修改

--- frontend/pages/theme/theme-list.js
+++ frontend/pages/theme/theme-list.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import ThemeBranches from '@/components/UI/theme-branches'
 
 export default function ThemeList() {
-  return <></>
+  return (
+    <>
+      <div className="container">
+        {/* 分頁標籤 */}
+        <ThemeBranches />
+      </div>
+    </>
+  )
 }
