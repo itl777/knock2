16b880e55383dbe0b7512af2c2c122e23b7e328a
tsuncc <tsunmobile010109@gmail.com>
2024-06-27 20:39:59 +0800

    1. 更新 stepper 元件  2. 增加 divider 元件 3. 更新 checkout drawer UI

--- frontend/components/UI/input-stepper/index.js
+++ frontend/components/UI/input-stepper/index.js
@@ -1,16 +1,57 @@
 import { IoIosAdd, IoIosRemove } from 'react-icons/io'
 import styles from './input-stepper.module.css'
+import { useState } from 'react'
+import IconButton from '@mui/material/IconButton'
+import { styled } from '@mui/material/styles'
 
-export default function InputStepper({ stepperValue = 1 }) {
+export default function InputStepper({
+  minValue = 1,
+  maxValue = 10,
+  stepperValue = 1,
+}) {
+  const StepperButton = styled(IconButton)(({ theme }) => ({
+    backgroundColor: 'white',
+    width: '1.5rem',
+    height: '1.5rem',
+    border: '1px solid var(--input-grey)',
+    padding: '4px',
+    '&:hover': {
+      backgroundColor: 'var(--pri-3)',
+    },
+  }))
+
+  const [value, setValue] = useState(stepperValue)
+  console.log(value)
+
+  const handleIncrease = () => {
+    if (+value >= maxValue) {
+      return
+    } else {
+      const newStepperValue = +value + 1
+      setValue(newStepperValue)
+    }
+  }
+
+  const handleDecrease = () => {
+    if (+value <= minValue) {
+      return
+    } else {
+      const newStepperValue = +value - 1
+      setValue(newStepperValue)
+    }
+  }
+
   return (
     <div className={styles.stepperInputContainer}>
-      <button className={styles.stepperBtn}>
-        <IoIosAdd />
-      </button>
-      <div className={styles.stepperValue}>{stepperValue}</div>
-      <button className={styles.stepperBtn}>
-        <IoIosRemove />
-      </button>
+      <StepperButton>
+        <IoIosRemove onClick={handleDecrease} />
+      </StepperButton>
+
+      <div className={styles.stepperValue}>{value}</div>
+
+      <StepperButton>
+        <IoIosAdd onClick={handleIncrease} />
+      </StepperButton>
     </div>
   )
 }
--- frontend/components/page-components/orders/order-product-img-box/index.js
+++ frontend/components/page-components/orders/order-product-img-box/index.js
@@ -1,5 +1,7 @@
 import styles from './order-product-img-box.module.css'
+import { motion } from 'framer-motion';
 import NoData from '@/components/UI/no-data'
+
 
 export default function OrderProductImgBox({
   imgSrc = '',
@@ -11,8 +13,27 @@ export default function OrderProductImgBox({
   smallHeight = '6rem',
   smallRadius = '0.75rem',
 }) {
+  // return (
+  //   <div
+  //     className={styles.itemImgBox}
+  //     style={{
+  //       '--img-width': width,
+  //       '--img-height': height,
+  //       '--img-radius': radius,
+  //       '--small-img-width': smallWidth,
+  //       '--small-img-height': smallHeight,
+  //       '--small-img-radius': smallRadius,
+  //     }}
+  //   >
+  //     {imgSrc ? (
+  //       <img src={imgSrc} alt={imgAlt} />
+  //     ) : (
+  //       <NoData text="無商品圖" borderRadius="0rem" fontSize="14px" />
+  //     )}
+  //   </div>
+  // )
   return (
-    <div
+    <motion.div
       className={styles.itemImgBox}
       style={{
         '--img-width': width,
@@ -22,12 +43,19 @@ export default function OrderProductImgBox({
         '--small-img-height': smallHeight,
         '--small-img-radius': smallRadius,
       }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
     >
       {imgSrc ? (
-        <img src={imgSrc} alt={imgAlt} />
+        <motion.img src={imgSrc} alt={imgAlt} />
       ) : (
-        <NoData text="無商品圖" borderRadius="0rem" fontSize="14px" />
+        <NoData text="無商品圖" borderRadius="0rem" />
       )}
-    </div>
-  )
+    </motion.div>
+  );
 }
