055161b0f3fe8dc76954a2be14909262f35993b1
tsuncc <tsunmobile010109@gmail.com>
2024-06-28 09:24:15 +0800

    update order modal components

--- frontend/components/UI/basic-modal/index.js
+++ frontend/components/UI/basic-modal/index.js
@@ -1,39 +1,45 @@
 // components/UI/basic-modal.jsx
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import styles from './basic-modal.module.css'
+import BlackBtn from '../black-btn'
+import RecipientButtonEdit from '@/components/page-components/orders/recipient-button-edit'
+import RecipientButton from '@/components/page-components/orders/recipient-button'
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '640px',
+  height: '640px',
   bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+  borderRadius: '1rem',
+  boxShadow: ' var(--card-shadow)',
+  padding: '1.5rem',
+}
 
-export default function BasicModal({ open, handleClose }) {
+export default function BasicModal({
+  open,
+  handleClose,
+  modalTitle = 'Modal Title',
+}) {
   return (
     <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+      <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <h5>{modalTitle}</h5>
+          <div className={styles.modalBody}>
+            <RecipientButtonEdit />
+            <RecipientButtonEdit />
+            <RecipientButton btnText='新增收件人資料' iconType='add' bgtype='outline'/>
+          </div>
+          <div className={styles.btnStack}>
+            <BlackBtn btnText="關閉" />
+          </div>
         </Box>
       </Modal>
     </div>
-  );
+  )
 }
