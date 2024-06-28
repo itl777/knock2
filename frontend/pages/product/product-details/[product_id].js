578913b5aaea586efc085e8320653845265d0a4d
IT <sanboul713@gmail.com>
2024-06-28 14:12:31 +0800

    【pages】details page動態路由

--- frontend/pages/product/product-details/[product_id].js
+++ frontend/pages/product/product-details/[product_id].js
@@ -1,18 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Breadcrumb from '@/components/page-components/products/breadcrumb'
 import PdTabs from '@/components/page-components/products/details-section/pd-tabs/index'
 import PdFeatures from '@/components/page-components/products/details-section/pd-features'
 import IndexLayout from '@/components/layout'
+import { useRouter } from 'next/router'
 
 export default function ProductDetails() {
+  const router = useRouter()
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    console.log(router.query.product_id)
+    fetch(`http://127.0.0.1:3001/products/details/${router.query.product_id}`)
+      .then((r) => r.json())
+      .then((dbData) => {
+        console.log('data', dbData.rows)
+        setData(dbData.rows)
+      })
+  }, [router])
+
   return (
     <>
       <IndexLayout pageName="productDetails" background="light">
         <Breadcrumb />
 
-        <PdFeatures />
+        <PdFeatures data={data} />
 
-        <PdTabs />
+        <PdTabs data={data} />
       </IndexLayout>
 
       <style jsx global>
