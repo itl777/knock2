25fb70f288fbdc5544756ddeae64547bd6fb33ad
Ireneowo <renpinning@gmail.com>
2024-07-09 17:05:53 +0800

    item

--- frontend/components/page-components/themes/detail-section/item/item.js
+++ frontend/components/page-components/themes/detail-section/item/item.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import myStyle from './item.module.css'
+import { FaPeopleGroup } from 'react-icons/fa6'
 
 export default function Item() {
   return (
     <>
-      <div className={myStyle.item}></div>
+      <div className={myStyle.item}>
+        <div className="container">
+          <div className="row">
+            <div className="d-flex flex-wrap flex-column col-3">
+              <FaPeopleGroup className={myStyle.group} />
+              <div className={myStyle.p}>2 - 5 人</div>
+            </div>
+            <div className="d-flex flex-wrap flex-column align-items-center col-3">
+              <FaPeopleGroup className={myStyle.group} />
+              <div>2 - 5人</div>
+            </div>
+            <div className="d-flex flex-wrap flex-column align-items-center col-3">
+              <FaPeopleGroup className={myStyle.group} />
+              <div>2 - 5人</div>
+            </div>
+            <div className="d-flex flex-wrap flex-column align-items-end col-3">
+              <FaPeopleGroup className={myStyle.group} />
+              <div>2 - 5人</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={myStyle.price}></div>
     </>
   )
