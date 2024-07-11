5e419b622050cf6fa42a5b60e895d3f9e8539444
ez003612 <ez003612@gmail.com>
2024-07-11 15:29:16 +0800

    頭像樣式

--- frontend/components/page-components/user/user-profile-form/avatar.js
+++ frontend/components/page-components/user/user-profile-form/avatar.js
@@ -1,17 +1,52 @@
 import styled from '@emotion/styled'
 import { API_SERVER } from '@/configs/api-path'
+import Image from 'next/image'
+import { FaCloudUploadAlt } from 'react-icons/fa'
 
 export default function AvatarFormItem({ avatar = '', open }) {
-  const AvatarFormItem = styled.button`
+  const Avatar = styled.button`
+    position: relative;
     width: 250px;
     height: 250px;
     border-radius: 125px;
-    background: ${avatar !== '' ? `url('${API_SERVER}/avatar/${avatar}')` : ''}
-      no-repeat #222222;
-    background-size: contain;
-    background-position: center;
+    background-color: #222222;
+    border: 1px solid #222222;
     box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.25);
   `
+  const UploadIcon = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    inset: 0;
+    color: white;
+    border-radius: 125px;
+    font-size: 50px;
+    filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.5));
+    opacity: 0;
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      opacity: 1;
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+  `
 
-  return <AvatarFormItem onClick={open} />
+  return (
+    <Avatar onClick={open}>
+      <Image
+        src={avatar !== '' ? `${API_SERVER}/avatar/${avatar}` : ''}
+        fill
+        alt="avatar"
+        style={{
+          objectFit: 'contain',
+          borderRadius: '100%',
+        }}
+      />
+      <UploadIcon>
+        <FaCloudUploadAlt />
+      </UploadIcon>
+    </Avatar>
+  )
 }
