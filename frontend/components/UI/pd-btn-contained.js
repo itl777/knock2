52b0277c045edc1de12f792bde4ece432b0873ff
IT <sanboul713@gmail.com>
2024-07-11 15:21:02 +0800

    【feat】[product details] 調整RWD 1.features 2.btn-group 3.category-group

--- frontend/components/UI/pd-btn-contained.js
+++ frontend/components/UI/pd-btn-contained.js
@@ -2,8 +2,15 @@ import { Button } from '@mui/material'
 // 改顏色
 import { ThemeProvider } from '@mui/material/styles'
 import theme from '@/context/theme'
+import useScreenSize from '@/hooks/useScreenSize'
+
 
 export default function PdBtnContained({ btnText = '沒設定', color = 'grey', onClick}) {
+  const userClientWidth = useScreenSize()
+  const btnPadding = userClientWidth > 576 ? '10px 19px' : '4px 10px'
+  const btnWidth = userClientWidth > 576 ? '11.25rem' : 'auto'
+  const btnFontSize = userClientWidth > 576 ? '18px' : '14px'
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -15,11 +22,11 @@ export default function PdBtnContained({ btnText = '沒設定', color = 'grey', 
             fontFamily: 'Noto Serif JP',
             borderRadius: '16px',
             marginLeft: '0.625rem',
-            fontSize: '18px',
+            fontSize: {btnFontSize},
             fontWeight: 700,
             letterSpacing: '2.16px',
-            width: '180px',
-            padding: '10px 19px',
+            width: {btnWidth},
+            padding: {btnPadding},
           }}
         >
           {btnText}
--- frontend/components/UI/pd-btn-outlined.js
+++ frontend/components/UI/pd-btn-outlined.js
@@ -2,8 +2,14 @@ import { Button } from '@mui/material'
 // 改顏色
 import { ThemeProvider } from '@mui/material/styles'
 import theme from '@/context/theme'
+import useScreenSize from '@/hooks/useScreenSize'
 
 export default function PdBtnOutlined({ btnText = '沒設定', onClick }) {
+  const userClientWidth = useScreenSize()
+  const btnPadding = userClientWidth > 576 ? '10px 19px' : '4px 10px'
+  const btnWidth = userClientWidth > 576 ? '11.25rem' : 'auto'
+  const btnFontSize = userClientWidth > 576 ? '18px' : '14px'
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -17,11 +23,11 @@ export default function PdBtnOutlined({ btnText = '沒設定', onClick }) {
             borderRadius: '16px',
             // borderColor: '#BA0606',
             marginLeft: '0.625rem',
-            fontSize: '18px',
+            fontSize: { btnFontSize },
             fontWeight: 700,
             letterSpacing: '2.16px',
-            width: '180px',
-            padding: '10px 19px',
+            width: { btnWidth },
+            padding: { btnPadding },
             ':hover': {
               // borderColor: 'rgba(186, 6, 6, 0.70)',
             },
