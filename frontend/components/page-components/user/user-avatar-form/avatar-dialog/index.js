b77ab34839ff5597b5448709b182f9b39b7e3ed1
ez003612 <ez003612@gmail.com>
2024-07-12 11:55:35 +0800

    前端 完成 頭像裁切功能＋拖放上傳功能

---
+++ frontend/components/page-components/user/user-avatar-form/avatar-dialog/index.js
@@ -0,0 +1,80 @@
+import { styled } from '@mui/material/styles'
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  IconButton,
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import BlackBtn from '@/components/UI/black-btn'
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}))
+export default function AvatarDialog({
+  children,
+  cropper,
+  setCropper,
+  setImgUrl,
+  imageRef,
+  setZoomValue,
+  openDialog,
+  closeDialog,
+  avatarSubmit,
+}) {
+  return (
+    <>
+      <BootstrapDialog
+        onClose={() => {
+          closeDialog()
+          if (cropper) cropper.destroy()
+          setCropper(null)
+          setImgUrl('')
+          imageRef.current = null
+          setZoomValue(0.1)
+        }}
+        aria-labelledby="customized-dialog-title"
+        open={openDialog}
+        fullWidth={true}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          <p>編輯頭像</p>
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={closeDialog}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <form>
+          <DialogContent
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '20px',
+            }}
+          >
+            {children}
+          </DialogContent>
+          <DialogActions>
+            <BlackBtn btnText="確認上傳" href="" onClick={avatarSubmit} />
+          </DialogActions>
+        </form>
+      </BootstrapDialog>
+    </>
+  )
+}
