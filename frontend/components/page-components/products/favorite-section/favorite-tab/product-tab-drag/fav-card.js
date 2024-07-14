f6931290f837bf5a33d81485a61036e17059ff2c
IT <sanboul713@gmail.com>
2024-07-15 01:28:44 +0800

    【feat】[product favorite] 串圖片

--- frontend/components/page-components/products/favorite-section/favorite-tab/product-tab-drag/fav-card.js
+++ frontend/components/page-components/products/favorite-section/favorite-tab/product-tab-drag/fav-card.js
@@ -1,11 +1,21 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import { FaXmark, FaCartShopping } from 'react-icons/fa6'
 import { useProduct } from '@/context/product-context'
 import myStyle from './drag.module.css'
 import DeleteIconBtn from '../product-tab-list/delete-icon-btn'
+import { useProductImg } from '@/hooks/useProductImg'
+import { PRODUCT_IMG } from '@/configs/api-path'
 
 export default function FavCard({ dbData }) {
   const { cardChange, setCardChange } = useProduct()
+  const [isId, setIsId] = useState(0)
+
+  const { data } = useProductImg(isId)
+  useEffect(() => {
+    setIsId(dbData.product_id)
+  }, [dbData])
+
   return (
     <>
       <div
@@ -14,14 +24,24 @@ export default function FavCard({ dbData }) {
         className="pd-card d-flex position-relative mb-2"
       >
         <div className="img-div position-relative">
-          <Image
+          {data && (
+            <Image
+              src={`${PRODUCT_IMG}/${data[0]}.jpg`}
+              width={81}
+              height={112}
+              style={{ width: '100%', height: '100%' }}
+              className={myStyle.imgStyle}
+              alt="..."
+            />
+          )}
+          {/* <Image
             src="/products/tea3.jpg"
             width={81}
             height={113}
             className={myStyle.imgStyle}
             draggable="false"
             alt=""
-          />
+          /> */}
           <div className={myStyle.xStyle}>
             <DeleteIconBtn
               cardChange={cardChange}
--- frontend/components/page-components/products/favorite-section/favorite-tab/product-tab-list/fav-card-large.js
+++ frontend/components/page-components/products/favorite-section/favorite-tab/product-tab-list/fav-card-large.js
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import BlackBtn from '@/components/UI/black-btn'
 import myStyle from './list.module.css'
 import DeleteIconBtn from './delete-icon-btn'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useProduct } from '@/context/product-context'
+import { useProductImg } from '@/hooks/useProductImg'
+import { PRODUCT_IMG } from '@/configs/api-path'
 
 export default function FavCardLarge({ dbData }) {
   const { cardChange, setCardChange } = useProduct()
+  const [isId, setIsId] = useState(0)
 
   const imgStyle = {
     width: ' 100%',
@@ -14,22 +17,36 @@ export default function FavCardLarge({ dbData }) {
     borderRadius: '12px 12px 0 0',
   }
 
+  const { data } = useProductImg(isId)
+  useEffect(() => {
+    setIsId(dbData.product_id)
+  }, [dbData])
+
   return (
     <>
       <div
         draggable="true"
-        id={dbData.product_name}
         className={`${myStyle['pd-card']} pd-card position-relative mb-2`}
       >
         <div className={`${myStyle['img-div']}`}>
-          <Image
+          {data && (
+            <Image
+              src={`${PRODUCT_IMG}/${data[0]}.jpg`}
+              width={258}
+              height={188}
+              style={{ width: '100%', height: 'auto',borderRadius:'12px 12px 0 0' }}
+              className="card-img-top"
+              alt="..."
+            />
+          )}
+          {/* <Image
             src="/products/p1-1.jpg"
             width={81}
             height={113}
             style={imgStyle}
             draggable="false"
             alt="..."
-          />
+          /> */}
           <DeleteIconBtn
             cardChange={cardChange}
             setCardChange={setCardChange}
