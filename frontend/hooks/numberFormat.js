1cbcd26d3078dad99cb8d0a43b6012396321bbaa
tsuncc <tsunmobile010109@gmail.com>
2024-07-15 17:15:56 +0800

    bugfix：使用 formatPrice(99) 會自動 + 1

--- frontend/hooks/numberFormat.js
+++ frontend/hooks/numberFormat.js
@@ -1,12 +1,14 @@
 export const formatIntlNumber = (value) => {
-  return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(
-    value
-  )
+  return new Intl.NumberFormat('en-IN', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(value)
 }
 
 export const formatPrice = (value) => {
-  const IntlNumber = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(
-    value
-  )
+  const IntlNumber = new Intl.NumberFormat('en-IN', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(value)
   return `$ ${IntlNumber}`
 }
