6a5d06c8dc5533482b5792c239e77ecb5e9af343
tsuncc <tsunmobile010109@gmail.com>
2024-07-23 11:48:30 +0800

    reservation result backend 修正無法顯示 branch name 問題

--- backend/routes/reservations.js
+++ backend/routes/reservations.js
@@ -232,7 +232,8 @@ router.get("/result/:reservation_id", async (req, res) => {
         r.cancel,
         r.created_at
         FROM reservations r
-        LEFT JOIN branches b ON b.branch_id = r.branch_themes_id
+        LEFT JOIN branch_themes bt ON bt.branch_themes_id = r.branch_themes_id
+        LEFT JOIN branches b ON b.branch_id = bt.branch_id
         JOIN themes t ON t.theme_id = r.branch_themes_id
         JOIN reservation_status rs ON rs.id = r.reservation_status_id
         JOIN sessions s ON s.sessions_id = r.session_id
