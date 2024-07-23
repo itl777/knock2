b18948f4ede1a1a05c8b648ae4fdbdd608ad505f
Ireneowo <renpinning@gmail.com>
2024-07-23 09:56:30 +0800

    我要預約按鈕更新

--- frontend/components/page-components/themes/detail-section/index.js
+++ frontend/components/page-components/themes/detail-section/index.js
@@ -1,15 +1,16 @@
 // @/components/page-components/themes/detail-section.js
 import React from 'react'
-import myStyle from './details.module.css'
 import MoreThemes from '@/components/page-components/themes/detail-section/more-themes'
+import BookingBtn from './booking-btn'
 
 export default function DetailSection({ Banner, Item, Step, Calendar }) {
   return (
     <>
       {Banner}
       {Item}
-      {Step}
+      <div id="step-section">{Step}</div>
       {Calendar}
+      <BookingBtn targetId="step-section" />
       <MoreThemes />
     </>
   )
